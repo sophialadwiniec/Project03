84ed922e0870d7f68035924047ef7de009e11ebf
sophialadwiniec <slladwiniec@dons.usfca.edu>
2021-05-06 12:16:32 -0700

    cleaning up code

--- sketch.js
+++ sketch.js
@@ -1,18 +1,16 @@
 var screenManager;
 
-
 function preload() {
   screenManager = new screenSetup('data/textTable.csv', 'data/buttonTable.csv'); 
 }
 
-// Setup the adventure manager
+
 function setup() {
   createCanvas(1280, 720);
   screenManager.setup(); 
   
 }
 
-// Adventure manager handles it all!
 function draw() {
   screenManager.draw();
   var buttonList =  screenManager.currentState.buttonList;
@@ -23,24 +21,12 @@ function draw() {
     buttonList[i].onOutside = clickableButtonOnOutside; 
   }
 }
-// function setupClickables() {
-//   // All clickables to have same effects
-//   for( let i = 0; i < clickables.length; i++ ) {
-//     clickables[i].onHover = clickableButtonHover;
-//     clickables[i].onOutside = clickableButtonOnOutside;    
-//   }
-// }
 
-// tint when mouse is over
 clickableButtonHover = function () {
   this.color = "#D99AAB";
-  // this.noTint = false;
-  // this.tint = "#FF0000";
 }
 
-// color a light gray if off
 clickableButtonOnOutside = function () {
-  // backto our gray color
   this.color = "#0889A6";
 }
 
